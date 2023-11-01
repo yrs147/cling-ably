7b8a9c5b86563e3379bb5c7e1b773e48473a265b
Yash Raj <yashraj14700728@gmail.com>
2023-11-01 22:43:32 +0530

    Added Different Colors for each user

--- internal/chat/chat.go
+++ internal/chat/chat.go
@@ -34,69 +34,62 @@ func InitializeClient(username string) (*ably.Realtime, error) {
 func SubscribeToChat(client *ably.Realtime, roomName, username string, lang string) {
 	channel := client.Channels.Get(roomName)
 
+	fmt.Printf("🚀 Welcome to %s!\n", roomName)
+	fmt.Printf("👉 Enter your message below: (type 'exit' to leave)\n")
+
+	memberColors := make(map[string]int) // Map to store member colors
+
 	_, err := channel.SubscribeAll(context.Background(), func(msg *ably.Message) {
-		// Check if the message is not from the current user
 		if msg.ClientID != username {
-			text,err := translate.Translate(msg.Data,lang)
-			if err!=nil {
-				fmt.Print(err)
+			text, err := translate.Translate(msg.Data, lang)
+			if err != nil {
+				fmt.Println(err)
+				return
 			}
-			fmt.Printf("[%s]: %s\n", msg.ClientID, text)
+			
+			// Determine color for the member
+			color, exists := memberColors[msg.ClientID]
+			if !exists {
+				// Assign a random color for the member
+				color = len(memberColors) + 1
+				memberColors[msg.ClientID] = color
+			}
+			
+			// Use color escape codes for different colors
+			colorCode := 31 + color // Start from color code 32 (red)
+			
+			// Clear the input line and print the message with the chosen color
+			fmt.Printf("\033[2K\r\033[%dm👉 [%s]: %s\n\033[0m👉 You: ", colorCode, msg.ClientID, text)
 		}
 	})
+
 	if err != nil {
-		err := fmt.Errorf("subscribing to channel: %w", err)
-		fmt.Println(err)
+		fmt.Printf("Error subscribing to channel: %v\n", err)
 	}
 
-	// Let the user send messages
 	publishing(channel)
 }
 
-
-func subscribeToEvent(channel *ably.RealtimeChannel, username string) (func(), error) {
-	unsubscribe, err := channel.Subscribe(context.Background(), "message", func(msg *ably.Message) {
-		if msg.ClientID != username {
-			// Display received messages from other users
-			fmt.Printf("[%s]: %v\n", msg.ClientID, msg.Data)
-		}
-	})
-	if err != nil {
-		fmt.Printf("Error subscribing to channel: %v\n", err)
-		return nil, err
-	}
-
-	// Subscribe to presence events (people entering and leaving) on the channel
-	_, pErr := channel.Presence.SubscribeAll(context.Background(), func(msg *ably.PresenceMessage) {
-		if msg.Action == ably.PresenceActionEnter {
-			fmt.Printf("%v has entered the chat\n", msg.ClientID)
-		} else if msg.Action == ably.PresenceActionLeave {
-			fmt.Printf("%v has left the chat\n", msg.ClientID)
-		}
-	})
-	if pErr != nil {
-		err := fmt.Errorf("subscribing to presence in channel: %w", pErr)
-		fmt.Println(err)
-	}
-
-	return unsubscribe, nil
-}
-
 func publishing(channel *ably.RealtimeChannel) {
-	
+	fmt.Print("👉 You: ")
+
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		text, _ := reader.ReadString('\n')
 		text = strings.ReplaceAll(text, "\n", "")
-		// final := translate.TranslateMsg(text,"en")
 
-		// Publish the message typed in to the Ably Channel
-		err := channel.Publish(context.Background(), "message", text)
-		// await confirmation that message was received by Ably
-		if err != nil {
-			err := fmt.Errorf("publishing to channel: %w", err)
-			fmt.Println(err)
+		if text == "exit" {
+			break
 		}
+
+		err := channel.Publish(context.Background(), "message", text)
+		if err != nil {
+			fmt.Printf("Error publishing message: %v\n", err)
+		}
+		// Overwrite the input line and keep it intact
+		// fmt.Printf("\033[2K\r👉 You: %s", text)
 	}
+	// The chat loop has ended, print a new input line
+	fmt.Printf("\033[2K\r👉 Enter your message below: (type 'exit' to leave)\n")
 }
